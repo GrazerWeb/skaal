b883cb91b7364c53b095d1658bb1b808c276db31
carljdp <carljdp@gmail.com>
2020-04-28 14:29:38 +0200

    find and print images

--- skaal.sh
+++ skaal.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # C O N S T S
 #-------------
 # File extentions for static (non-animated) rasterized (non-vector) images
@@ -13,3 +14,30 @@ declare -g -r TIFF='tif|tiff'
 declare -g -r WEBP='webp'
 declare -g -r AllImageExtensions="\.($BMP|$GIF|$JP2|$JPEG|$MPEG|$PNG|$TIFF|$WEBP)$"
 
+
+# F U N C S
+#-------------
+
+function findRasterImageFiles () {
+    declare -r lookInDir="${1:-.}" 
+    echo "$( find $lookInDir | egrep -i $AllImageExtensions )"
+}
+
+
+# A R G S
+#-------------
+declare -g -r lookInDir="${1:-.}" 
+
+
+# M A I N
+#-------------
+echo -e '\n ◲ skaal.sh [ start ]\n'
+
+# Find all images and print with dimensions
+findRasterImageFiles "$lookInDir" | xargs -d '\n' identify -ping -format "%w %h %i\n"
+
+
+# E X I T
+#-------------
+echo -e '\n ◲ skaal.sh [ end ]\n'
+exit 0
